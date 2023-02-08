8b4b56a672f7203a8737739919321b1db54fa413
Sydney Cendana <sydneycendana@gmail.com>
2023-02-07 19:22:47 -0800

    user static methods

--- authenticate-me/backend/db/models/user.js
+++ authenticate-me/backend/db/models/user.js
@@ -1,15 +1,37 @@
 "use strict";
+const bcrypt = require("bcryptjs");
 const { Model, Validator } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    toSafeObject() {
+      const { id, username, email } = this;
+      return { id, username, email };
     }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id) {
+      return User.scope("currentUser").findByPk(id);
+    }
+
+    static async login({ credential, password }) {
+      const { Op } = require("sequelize");
+      const user = await User.scope("loginUser").findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential,
+          },
+        },
+      });
+
+      if (user && user.validatePassword(password)) {
+        return User.scope("currentUser").findByPk(user.id);
+      }
+    }
+    static associate(models) {}
   }
   User.init(
     {
