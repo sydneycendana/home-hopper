b768a327c27ec44437a50889e428fcef4fcc873b
Sydney Cendana <sydneycendana@gmail.com>
2023-02-12 12:59:10 -0800

    added options to users migration

--- backend/db/migrations/20230208010711-create-user.js
+++ backend/db/migrations/20230208010711-create-user.js
@@ -8,46 +8,50 @@ if (process.env.NODE_ENV === "production") {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Users", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Users",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        firstName: {
+          type: Sequelize.STRING,
+        },
+        lastName: {
+          type: Sequelize.STRING,
+        },
+        username: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING(30),
+        },
+        email: {
+          allowNull: false,
+          unique: true,
+          type: Sequelize.STRING(256),
+        },
+        hashedPassword: {
+          allowNull: false,
+          type: Sequelize.STRING.BINARY,
+        },
+        createdAt: {
+          allowNull: false,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+          type: Sequelize.DATE,
+        },
       },
-      firstName: {
-        type: Sequelize.STRING,
-      },
-      lastName: {
-        type: Sequelize.STRING,
-      },
-      username: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(30),
-      },
-      email: {
-        allowNull: false,
-        unique: true,
-        type: Sequelize.STRING(256),
-      },
-      hashedPassword: {
-        allowNull: false,
-        type: Sequelize.STRING.BINARY,
-      },
-      createdAt: {
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Users");
+    await queryInterface.dropTable("Users", options);
   },
 };
