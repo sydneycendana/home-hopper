edc7b7d266f21878450600ac54089e4437cfa821
Sydney Cendana <sydneycendana@gmail.com>
2023-02-14 16:55:50 -0800

    added constraints to review migration file

---
+++ backend/db/migrations/20230215005309-create-review.js
@@ -0,0 +1,51 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Reviews",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          references: { model: "Users" },
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+          references: { model: "Spots" },
+        },
+        review: {
+          type: Sequelize.STRING,
+        },
+        stars: {
+          type: Sequelize.INTEGER,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      },
+      options
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Reviews", options);
+  },
+};
