262a28001da9ed882aa45e624d2bcd334d09fc51
Sydney Cendana <sydneycendana@gmail.com>
2023-02-16 15:07:29 -0800

    changed validation file

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,10 +1,37 @@
+// const { validationResult } = require("express-validator");
+
+// const handleValidationErrors = (req, _res, next) => {
+//   const validationErrors = validationResult(req);
+
+//   if (!validationErrors.isEmpty()) {
+//     const errors = validationErrors.array().map((error) => `${error.msg}`);
+
+//     const err = Error("Bad request.");
+//     err.errors = errors;
+//     err.status = 400;
+//     err.title = "Bad request.";
+//     next(err);
+//   }
+//   next();
+// };
+
+// module.exports = {
+//   handleValidationErrors,
+// };
+
+// backend/utils/validation.js
 const { validationResult } = require("express-validator");
 
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
 const handleValidationErrors = (req, _res, next) => {
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
-    const errors = validationErrors.array().map((error) => `${error.msg}`);
+    const errors = {};
+    validationErrors
+      .array()
+      .forEach((error) => (errors[error.param] = error.msg));
 
     const err = Error("Bad request.");
     err.errors = errors;
