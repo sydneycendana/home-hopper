5fbf4d72d9beb086b37f583ab1f7568b87c7cc10
Sydney Cendana <sydneycendana@gmail.com>
2023-02-20 13:26:13 -0800

    added sanitizers to spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,6 +9,36 @@ const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
 
+const validateSpot = [
+  check("ownerId").exists({ checkFalsy: true }).withMessage("Invalid owner"),
+  check("address")
+    .exists({ checkFalsy: true })
+    .withMessage("Street address is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
+  check("country")
+    .exists({ checkFalsy: true })
+    .withMessage("Country is required"),
+  check("lat")
+    .exists({ checkFalsy: true })
+    .toFloat()
+    .isDecimal()
+    .withMessage("Latitude is not valid"),
+  check("lng")
+    .exists({ checkFalsy: true })
+    .toFloat()
+    .isDecimal()
+    .withMessage("Longitude is not valid"),
+  check("name")
+    .exists({ checkFalsy: true })
+    .isLength({ max: 50 })
+    .withMessage("Name must be less than 50 characters"),
+  check("price")
+    .exists({ checkFalsy: true })
+    .withMessage("Price per day is required"),
+  handleValidationErrors,
+];
+
 // get all spots **STILL NEED AVGRATING AND PREVIEW IMAGE**
 router.get("/", async (req, res, next) => {
   const spots = await Spot.findAll();
