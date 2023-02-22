805ffe4c231f00b7aafed41300abc04cc7f6604b
Sydney Cendana <sydneycendana@gmail.com>
2023-02-21 22:13:08 -0800

    delete spot route complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -263,6 +263,32 @@ router.put("/:spotId", requireAuth, validateSpot, async (req, res, next) => {
   }
 });
 
+router.delete("/:spotId", requireAuth, async (req, res, next) => {
+  const spotId = req.params.spotId;
+  const ownerId = req.user.id;
+
+  const spot = await Spot.findOne({
+    where: {
+      id: spotId,
+      ownerId: ownerId,
+    },
+  });
+
+  if (!spot) {
+    return res.status(404).json({
+      message: "Spot couldn't be found",
+      statusCode: 404,
+    });
+  }
+
+  await spot.destroy();
+
+  res.status(200).json({
+    message: "Successfully deleted",
+    statusCode: 200,
+  });
+});
+
 //add image to spot by spot id
 router.post("/:spotId/images", requireAuth, async (req, res, next) => {
   const { url, preview } = req.body;
