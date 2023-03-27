2362edb81cbdb257df02228a42fbbf08e248710e
Sydney Cendana <sydneycendana@Sydneys-MBP-2.lan>
2023-03-27 12:06:56 -0700

    sign up and login working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { useDispatch } from "react-redux";
 import { Route, Switch } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
+import SignUpFormPage from './components/SignUpFormPage'
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -9,16 +10,24 @@ function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
+useEffect(() => {
+dispatch(sessionActions.restoreUser())
+setIsLoaded(true)
+},[dispatch])
 
-  return isLoaded && (
-    <Switch>
-      <Route path="/login">
-        <LoginFormPage />
-      </Route>
-    </Switch>
+return (
+    <>
+      {isLoaded && (
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignUpFormPage />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -11,11 +11,20 @@ const setUser = (user) => {
   };
 };
 
-// const removeUser = () => {
-//   return {
-//     type: REMOVE_USER,
-//   };
-// };
+const removeUser = () => {
+  return {
+    type: REMOVE_USER,
+  };
+};
+
+//thunk action
+export const restoreUser = () => async dispatch => {
+  const response = await csrfFetch('/api/session');
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 
 //thunk action
 export const login = (user) => async (dispatch) => {
@@ -33,12 +42,33 @@ export const login = (user) => async (dispatch) => {
 };
 
 //thunk action
-export const restoreUser = () => async dispatch => {
-  const response = await csrfFetch('/api/session');
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+//thunk action
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
   const data = await response.json();
+  console.log("data ----->", data)
   dispatch(setUser(data.user));
   return response;
 };
+
 
 const initialState = { user: null };
 
