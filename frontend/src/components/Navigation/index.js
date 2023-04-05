4f6a9507e8e5c902bea0365c25a9b719a8a61a4e
Sydney Cendana <sydneycendana@Sydneys-MBP-2.lan>
2023-04-05 14:36:10 -0700

    added logo

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -3,6 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
+import logo from '../../images/logo.png'
+import shortLogo from '../../images/logo-short.png'
+
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
@@ -10,7 +13,9 @@ function Navigation({ isLoaded }){
   return (
     <ul className="container">
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/">
+          <img className="logo" src={logo} alt="logo"/>
+        </NavLink>
       </li>
       {isLoaded && (
         <li>
