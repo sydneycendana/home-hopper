fc10f66447b351015003c6ec84f6d915e1af85ac
Sydney Cendana <103895652+sydneycendana@users.noreply.github.com>
2023-07-18 13:37:36 -0400

    responsive

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,10 +1,10 @@
 // frontend/src/components/Navigation/ProfileButton.js
 import React, { useState, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import * as sessionActions from '../../store/session';
-import OpenModalButton from '../OpenModalButton';
-import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from "../../store/session";
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import { useHistory } from "react-router-dom";
 import { NavLink, Link } from "react-router-dom";
 import { getSpotsThunk } from "../../store/spots";
@@ -13,11 +13,11 @@ function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const history = useHistory();
   const [showMenu, setShowMenu] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
   const ulRef = useRef();
 
-  const sessionUser = useSelector(state => state.session.user);
+  const sessionUser = useSelector((state) => state.session.user);
   const spots = useSelector((state) => state.spot.allSpots);
-
 
   const openMenu = () => {
     if (showMenu) return;
@@ -33,21 +33,34 @@ function ProfileButton({ user }) {
       }
     };
 
-    document.addEventListener('click', closeMenu);
+    document.addEventListener("click", closeMenu);
 
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 55em)");
+    setIsSmallScreen(mediaQuery.matches);
+
+    const handleResize = (e) => {
+      setIsSmallScreen(e.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => mediaQuery.removeEventListener("change", handleResize);
+  }, []);
+
   const logout = (e) => {
     e.preventDefault();
-    history.push("/")
+    history.push("/");
     dispatch(sessionActions.logout());
   };
 
   const handleManageSpots = () => {
-    history.push('/spots/current');
+    history.push("/spots/current");
     setShowMenu(false);
-  }
+  };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
@@ -63,14 +76,23 @@ function ProfileButton({ user }) {
             <li>Hello, {user.firstName}</li>
             <li>{user.email}</li>
             {sessionUser && (
-            <li onClick={handleManageSpots}>
-              <NavLink exact to="/spots/current">
-                Manage Spots
-              </NavLink>
-            </li>
+              <li onClick={handleManageSpots}>
+                <NavLink exact to="/spots/current">
+                  Manage Spots
+                </NavLink>
+              </li>
+            )}
+            {sessionUser && isSmallScreen && (
+              <li>
+                <NavLink exact to="/spots/new">
+                  Create a New Spot
+                </NavLink>
+              </li>
             )}
             <li>
-              <button className="profile-dropdown__logout" onClick={logout}>Log Out</button>
+              <button className="profile-dropdown__logout" onClick={logout}>
+                Log Out
+              </button>
             </li>
           </>
         ) : (
